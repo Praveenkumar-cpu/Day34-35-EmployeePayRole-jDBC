be127a5eb31a7300cbbe772e79805e4db9ba292f
praveen <praveenpkbadiger@gmail.com>
2022-09-18 11:25:45 +0530

    Tested-Givenrange data

--- test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -12,9 +12,9 @@ public class EmployeePayrollServiceTest {
 
     @Test
     public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
-        EmployeePayrollData[] arrayOfEmp = {new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+        EmployeePayrollData[] arrayOfEmp = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
                 new EmployeePayrollData(2, "Bill Gates", 200000.0),
-                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)};
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
         EmployeePayrollService employeePayrollService;
         employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
         employeePayrollService.writeEmployeeData(EmployeePayrollService.IOService.FILE_IO);
@@ -53,5 +53,16 @@ public class EmployeePayrollServiceTest {
         System.out.println(employeePayrollData);
     }
 
+    @Test
+    public void givenDateRangeForEmployee_WhenRetrievedUsingStatement_ShouldReturnProperData()
+            throws EmployeePayrollException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readData(EmployeePayrollService.IOService.DB_IO);
+        List<EmployeePayrollData> employeeDataInGivenDateRange = employeePayrollService
+                .getEmployeesInDateRange("2018-01-01", "2019-11-15");
+        assertEquals(2, employeeDataInGivenDateRange.size());
+        System.out.println(employeePayrollData);
     }
+
+}
 
