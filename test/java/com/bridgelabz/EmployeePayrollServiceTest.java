ed69f7db1322c63851d05355f75b574279b11952
praveen <praveenpkbadiger@gmail.com>
2022-09-18 10:23:51 +0530

    Tested: test-cases

--- test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -1,2 +1,46 @@
-package com.bridgelabz;public class EmployeePayrollServiceTest {
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmployeePayrollServiceTest {
+
+    @Test
+    public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmp = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+        employeePayrollService.writeEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        List<EmployeePayrollData> employeeList = employeePayrollService.readData(EmployeePayrollService.IOService.FILE_IO);
+        System.out.println(employeeList);
+        assertEquals(3, entries);
+    }
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readData(EmployeePayrollService.IOService.DB_IO);
+        assertEquals(3, employeePayrollData.size());
+    }
+
+    @Test
+
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDatabase() throws EmployeePayrollException {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readData(EmployeePayrollService.IOService.DB_IO);
+        employeePayrollService.updateEmployeeSalary("Mark Zuckerberg", 3000000.00);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Mark Zuckerberg");
+        assertTrue(result);
+        System.out.println(employeePayrollData);
+    }
 }
+
